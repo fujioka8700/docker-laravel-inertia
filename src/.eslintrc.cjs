a6e75d794dfe2f272d5fa87f17ffade8bd07b5f5
fujioka <fujioka8700@gmail.com>
2024-01-03 11:34:38 +0900

    [add] add eslint for vue.

--- src/.eslintrc.cjs
+++ src/.eslintrc.cjs
@@ -5,10 +5,14 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
-    parser: '@typescript-eslint/parser'
+    sourceType: 'module'
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
   rules: {
     'vue/multi-word-component-names': 'off'
   },
