da1f53b35feaaa0744d34e45b93667923b8cddf1
fujioka <fujioka8700@gmail.com>
2024-01-03 11:16:57 +0900

    [add] add globals route true.

--- src/.eslintrc.cjs
+++ src/.eslintrc.cjs
@@ -1,10 +1,8 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -13,5 +11,8 @@ module.exports = {
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
   rules: {
     'vue/multi-word-component-names': 'off'
+  },
+  globals: {
+    route: true
   }
 }
