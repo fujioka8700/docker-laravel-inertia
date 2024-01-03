4b3a4118867e16711344a2320ca72e0454052384
fujioka <fujioka8700@gmail.com>
2024-01-03 10:09:57 +0900

    [add] add eslint recommended

--- src/.eslintrc.cjs
+++ src/.eslintrc.cjs
@@ -2,14 +2,15 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     parser: '@typescript-eslint/parser'
   },
-  extends: ['plugin:vue/vue3-essential'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
   rules: {
     'vue/multi-word-component-names': 'off'
   }
